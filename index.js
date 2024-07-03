25ab1baa98fd8226d3c7f8e5010f9e257019c57e
Manish Kumar <95423130+ProgramerSalar@users.noreply.github.com>
2024-07-03 17:26:39 +0530

    add data

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 9000
 
 
+
+
+
 server.use(middlewares)
 server.use(router)
+
 server.listen(port, () => {
     console.log("Json server is running.....")
 })
